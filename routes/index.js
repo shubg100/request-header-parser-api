4d2b131b5d2686b6a5e6c1be1d8ce588700d97e6
Shubham Gusain <shubg100@gmail.com>
2016-10-18 18:04:31 +0000

    app.js fixes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept");
+    res.header("Content-type: text/html; charset=utf-8");
+    next();
+});
+
+router.get('/api/whoami/', function(req, res) {
+    // req.socket.remoteAddress ||req.connection.socket.remoteAddress;
+    var obj = {
+        'ipaddress': req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        'language': req.headers["accept-language"].split(',')[0],
+        'software': req.headers['user-agent'].split(' (')[1].split(') ')[0]
+    };
+    res.json(obj); 
+});
+
 module.exports = router;
